14b4636b193948d6a3c2953fe1cafab910e3c1c6
Antoinette Andre <antoinette.andre2002@gmail.com>
2025-03-04 16:59:45 +0100

    Add BookingsController

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,37 @@
 class BookingsController < ApplicationController
+  before_action :set_offering, only: [:new, :create]
+
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(params[:booking])
+    @booking.offering = @offering
+    @booking.user = current_user
+
+    if @booking.save
+      redirect_to booking_path(@booking), notice: 'Booking request was successfully submitted.'
+    else
+      render :new
+    end
   end
 
   def index
+    @booking = current_user.bookings
   end
 
   def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date_start, :date_end, :name)
+  end
+
+  def set_offering
+    @offering = Offering.find(params[:offering_id])
   end
 end
