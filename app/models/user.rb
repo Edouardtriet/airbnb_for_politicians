9cbf852d072ca18da6931c3f8f7b6b0279621bf2
marc bernard <marc.bernard@ece-france.com>
2025-03-08 13:35:53 +0100

    it is now possible to upload a different profile picture or just delete it as well, the profile will have a placeholder, it doesn't brake anymore

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,17 @@ class User < ApplicationRecord
   has_many :listings, dependent: :destroy
   has_many :bookings, dependent: :destroy
   has_one_attached :profile_picture
+  attr_accessor :remove_profile_picture
+
+  before_save :check_remove_profile_picture
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+
+  def check_remove_profile_picture
+    self.profile_picture.purge if self.remove_profile_picture == '1'
+  end
 end
