19fbb96495e2566db90a9a288d5755072bfa1410
edouard triet <trietedouard@gmail.com>
2025-03-10 16:24:27 -0600

    added a dashboard page where the user can see his listings and offerings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:dashboard]
+
   def show
     @user = User.find(params[:id])
     @listings = @user.listings
+  end
+
+  def dashboard
+    @user = current_user
+    @listings = @user.listings if @user
+    @bookings = @user.bookings if @user
+    @received_bookings = Booking.joins(:listing).where(listings: { user_id: @user.id }) if @user
+
+    # If you want to debug, place it INSIDE the method
+    Rails.logger.debug "Current user in dashboard action: #{current_user.inspect}"
   end
 end
