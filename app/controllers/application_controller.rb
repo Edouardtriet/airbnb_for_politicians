d025647a064bccef6dac7b9953fe43e35aae0748
edouard triet <trietedouard@gmail.com>
2025-03-11 12:54:17 -0600

    solved the login bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:profile_picture])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile_picture])
     devise_parameter_sanitizer.permit(:account_update, keys: [:profile_picture, :remove_profile_picture, :name])
   end
 end
